7a978e2971c79f16a5d996ee874e3dcf4b0c34a1
Jaime Andres Salas <jsalasmoreira@uoc.edu>
2023-08-01 14:46:01 -0500

    Create a new actions

--- app/main.py
+++ app/main.py
@@ -1,39 +1,15 @@
-"""This script execute a simple web server with FastAPI process data with
-    - Cloud functions (read and write data in big query) 
-    - Dataflow with Beam (write historical data in big query)
-Endpoints:
-    - /upload: Receives historical data files in CSV format and uploads them to the new database.
-    - /batch_insert: Allows the insertion of batch transactions with 1 to 1000 rows in a single request
-    - /metrics/hired_employes_2021 : Retrieves metrics related to hired employees in 2021.
-    - /metrics/more_hired_2021 : Retrieves metrics related to the most hired employees for deparment in 2021.
-"""
+from typing import Union
 
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from fastapi.responses import JSONResponse
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
-from google.cloud import bigquery
-from src.bigquery_operator import OperatorBigQuery
+from fastapi import FastAPI
 
-names_tables = {'hired_employees','departments','jobs'}
-file_location = 'gs://globant-jasm-1/data_csv/'
-project_id = 'de-jasm-globant'
-dataset = 'globant_dataset'
 app = FastAPI()
-Operator = OperatorBigQuery(name_tables= names_tables, 
-                            files_location= file_location, 
-                            project_id=project_id, 
-                            dataset=dataset)
 
-"""Create a api for use Operator BigQuery class and execute the methods insert_batch and hired_employees_2021"""
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-@app.post("/batch_insert")
-async def create_upload_file():
-    Operator.insert_batch()
-    return {"message": "Files uploaded successfully"}
 
 
-
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
--- app/src/bigquery_operator.py
+++ app/src/bigquery_operator.py
@@ -4,32 +4,16 @@ import pandas as pd
 import os
 
 class OperatorBigQuery(ABC):
-    def __init__(self, name_tables:dir, files_location:str, project_id:str, dataset:str, **kwargs):
+    def __init__(self, name_tables, files_location:str, project_id:str, dataset:str):
         self.client = bigquery.Client()
         self.name_tables = name_tables
         self.files_location = files_location
         self.project_id = project_id
         self.dataset = dataset
     
-    @name_tables.setter
-    def name_tables(self,name_tables):
-        self._name_tables = name_tables
-    
-    @files_location.setter
-    def files_location(self,files_location):
-        self._files_location = files_location
-    
-    @project_id.setter
-    def project_id(self,project_id):
-        self._project_id = project_id
-    
-    @dataset.setter
-    def dataset(self,dataset):
-        self._dataset = dataset
-
     def insert_batch(self):
-        for name_table in self._name_tables:
-            table_name = self._project_id+'.'+self._dataset+'.'+name_table
+        for name_table in self.name_tables:
+            table_name = self.project_id+'.'+self.dataset+'.'+name_table
             df = pd.read_csv(self._files_location+name_table+'.csv')
             df.to_gbq(table_name, if_exists='append', chunksize=1000)
    
