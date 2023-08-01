b09233318f31859aa0ed2e0ddebd37183e518627
Jaime Andres Salas <jsalasmoreira@uoc.edu>
2023-08-01 16:41:49 -0500

    testing 3

--- app/src/bigquery_operator.py
+++ app/src/bigquery_operator.py
@@ -2,17 +2,19 @@ from abc import ABC
 from google.cloud import bigquery
 import pandas as pd
 import os
-schema = {
-    "hired_employees": {{"name": "id", 'type': 'INTEGER'},
+
+hired_employees = {{"name": "id", 'type': 'INTEGER'},
                         {"name": "name", 'type': 'STRING'},
                         {"name": "datetime", 'type':'STRING'},
                         {"name": "department_id", 'type': 'INTEGER'},
-                        {"name": "job_id", 'type': 'INTEGER'}},
-    "departments":  {{"name": "id", 'type': 'INTEGER'},
+                        {"name": "job_id", 'type': 'INTEGER'}}
+departments ={{"name": "id", 'type': 'INTEGER'},
                     {"name": "departments", 'type': 'STRING'},},
-    "jobs":         {{"name": "id", 'type': 'INTEGER'},
-                    {"name": "job", 'type': 'STRING'}}
-}
+jobs ={{"name": "id", 'type': 'INTEGER'},
+      {"name": "job", 'type': 'STRING'}}
+dict_tables = {'hired_employees': hired_employees,
+               'departments': departments,
+               'jobs': jobs}
 class OperatorBigQuery(ABC):
     def __init__(self, name_tables, files_location:str, project_id:str, dataset:str):
         self.client = bigquery.Client()
@@ -28,10 +30,11 @@ class OperatorBigQuery(ABC):
             df = pd.read_csv(location, sep=',',
                              encoding='utf-8',
                              header=None, 
+                             on_bad_lines = 'warn',
                              storage_options={"token": "cloud"})
             df.to_gbq(table_name, if_exists='append', 
                       chunksize=1000,
-                      table_schema=schema[name_table])
+                      table_schema=dict_tables[name_table])
    
     def hired_employees_2021(self):
         with open(os.path.abspath("./src/queries/hired_employees_2021.sql"), "r") as f:
