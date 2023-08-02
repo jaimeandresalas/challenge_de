573cfb94c2139e429917caf8c81e726d2a0bfa93
Jaime Andres Salas <jsalasmoreira@uoc.edu>
2023-08-02 08:54:31 -0500

    changing the insert_batch function

--- app/src/bigquery_operator.py
+++ app/src/bigquery_operator.py
@@ -6,8 +6,8 @@ import os
 hired_employees = {"id":int, "name":str,"datetime":str,
                    "department_id":int,"job_id":int}
 columns_hired_employees = ['id', 'name', 'datetime', 'department_id', 'job_id']
-departments ={ "id": int, "departments": str}
-columns_departments = ['id', 'departments']
+departments ={ "id": int, "department": str}
+columns_departments = ['id', 'department']
 jobs ={"id": int, "job": str}
 columns_jobs = ['id', 'job']
 tables_schema = {"hired_employees" : hired_employees, "jobs" : jobs,
@@ -27,19 +27,22 @@ class OperatorBigQuery(ABC):
         for name_table in self.name_tables:
             table_name = self.project_id+'.'+self.dataset+'.'+name_table
             location = self.files_location+name_table+'.csv'
-            df = pd.read_csv(location, sep=',',
+            with pd.read_csv(location, sep=',',
                              encoding='utf-8',
                              header=None,
                              names=columns_tables[name_table],
                              dtype=tables_schema[name_table], 
+                             chunksize=1000,
                              on_bad_lines = 'warn',
-                             storage_options={"token": "cloud"})
-            job_config = bigquery.LoadJobConfig(
+                             storage_options={"token": "cloud"}) as reader:
+                for chunk in reader:
+                    df = chunk
+                    job_config = bigquery.LoadJobConfig(
                          write_disposition="WRITE_APPEND",
                          autodetect = True
                          )
-            job = self.client.load_table_from_dataframe(df, table_name, job_config=job_config)
-            job.result()  
+                    job = self.client.load_table_from_dataframe(df, table_name, job_config=job_config)
+                    job.result()  
     
     def hired_employees_2021(self):
         with open(os.path.abspath("./src/queries/hired_employees_2021.sql"), "r") as f:
